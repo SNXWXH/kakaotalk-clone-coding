7de05bca978dc06d11356f4b8a6985417838edd2
SNXWXH <snxwxh@gmail.com>
2025-04-03 16:35:12 +0900

    design: input focus 시 border 처리

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,18 +1,18 @@
 export default function LoginPage() {
   return (
     <>
-      <div className='flex flex-col items-center w-[392px] h-[642px] bg-[#FFDF3D] text-sm rounded-xl'>
+      <div className='flex flex-col items-center w-[392px] h-[642px] bg-[#F9E000] text-sm rounded-xl'>
         <img src='../../public/kakaoLogo.png' className='w-1/3 mt-25 mb-10' />
 
         <input
           type='email'
           placeholder='아이디(E-mail)'
-          className='w-2/3 h-[45px] bg-[#F5F5F5] p-2 border-1 border-zinc-200'
+          className='w-2/3 h-[45px] bg-[#F5F5F5] p-2 border-1 border-zinc-200 focus:ring-1 focus:ring-inset focus:ring-gray-400 focus:outline-none'
         />
         <input
           type='password'
           placeholder='비밀번호'
-          className='w-2/3 h-[45px] bg-[#F5F5F5] p-2 border-1 border-zinc-200'
+          className='w-2/3 h-[45px] bg-[#F5F5F5] p-2 border-1 border-zinc-200 focus:ring-1 focus:ring-inset focus:ring-gray-400 focus:outline-none'
         />
         <button className='w-2/3 h-[45px] bg-[#F5F5F5] my-3'>로그인</button>
         <button>이메일로 회원가입</button>
