f771755155fca70011be6b56390ec8976b0c6bd2
SNXWXH <snxwxh@gmail.com>
2025-04-10 18:39:34 +0900

    design: chatListItem 컴포넌트 적용

--- src/pages/chatList/ChatList.tsx
+++ src/pages/chatList/ChatList.tsx
@@ -1,8 +1,25 @@
+import ChatListItem from '../../common/ChatListItem';
+
 function ChatList() {
   return (
     <>
-      <div className='flex flex-col items-center w-[392px] h-[642px] bg-[#F9E000] text-sm rounded-xl'>
-        chatList
+      <div className='flex flex-col w-[392px] h-[642px] bg-[#F9E000] text-sm rounded-xl'>
+        <div className='flex items-end m-6'>
+          <p className='text-4xl font-bold'>채팅</p>
+          <p className='ml-2 text-base'>서라</p>
+        </div>
+        <div className='overflow-auto'>
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+        </div>
       </div>
     </>
   );
