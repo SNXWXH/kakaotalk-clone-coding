67cb7fc398c2d974cd1d3fab4885adb5864a8eb3
SNXWXH <snxwxh@gmail.com>
2025-04-10 17:16:32 +0900

    design: 채팅 목록 컴포넌트 생성 및 라우터 설정

---
+++ src/pages/chatList/ChatList.tsx
@@ -0,0 +1,11 @@
+function ChatList() {
+  return (
+    <>
+      <div className='flex flex-col items-center w-[392px] h-[642px] bg-[#F9E000] text-sm rounded-xl'>
+        chatList
+      </div>
+    </>
+  );
+}
+
+export default ChatList;
