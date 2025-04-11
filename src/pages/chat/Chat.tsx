a3a42fe13161f01349df818466711f1e4707076d
SNXWXH <snxwxh@gmail.com>
2025-04-11 17:37:06 +0900

    design: 채팅창 헤더, 채팅, 입력창 UI 틀 생성

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -2,7 +2,12 @@ function Chat() {
   return (
     <>
       <div className='flex flex-col w-[392px] h-[642px] bg-[#c1d6e8] text-sm rounded-xl'>
-        채팅방
+        <header className='relative flex h-16 items-center border'>
+          <img src={'./left.svg'} className='absolute left-4 h-6' />
+          <p className='mx-auto text-xl font-bold'>이설아</p>
+        </header>
+        <div className='border h-3/4'>chat</div>
+        <div className='border h-1/5'>입력창</div>
       </div>
     </>
   );
