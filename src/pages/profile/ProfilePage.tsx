f736d707f1c1e0b641c7c061e6f056966ce212ef
SNXWXH <snxwxh@gmail.com>
2025-04-15 16:17:17 +0900

    design: profile 퍼블리싱
    배경화면 유무에 따른 bg 변경

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,10 +1,52 @@
+import { useNavigate } from 'react-router-dom';
+
 function ProfilePage() {
+  const profileBg = '/profileBg.png';
+  const navigate = useNavigate();
+
+  const navigateToChat = () => {
+    navigate('/chat');
+  };
+
   return (
-    <>
-      <div className='flex w-[392px] h-[642px] bg-[#FEFEFE] text-sm rounded-xl'>
-        profile
+    <div className='relative w-[392px] h-[642px] rounded-xl overflow-hidden text-sm'>
+      <div
+        className='absolute inset-0 z-0'
+        style={{
+          backgroundImage: profileBg ? `url(${profileBg})` : undefined,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        {profileBg && <div className='w-full h-full bg-white/30' />}
       </div>
-    </>
+      <div
+        className={`relative z-10 flex flex-col w-full h-full ${
+          profileBg ? '' : 'bg-gray-200'
+        } justify-end items-center p-3`}
+      >
+        <div className='flex flex-col justify-center items-center w-full'>
+          <img
+            src={'/profileImg.jpeg'}
+            className='w-28 aspect-square object-cover rounded-2xl mb-2'
+          />
+          <p className='text-xl font-bold my-1.5'>이설아</p>
+          <p className='text-base font-medium'>상태메세지야</p>
+          <hr className='w-5/6 border-t border-gray-500 my-4' />
+        </div>
+        <div className='flex justify-between items-center w-11/12 px-4 mb-2'>
+          <button
+            onClick={navigateToChat}
+            className='w-2/5 h-10 bg-gray-100 shadow-md rounded-md hover:bg-gray-200'
+          >
+            나와의 채팅
+          </button>
+          <button className='w-2/5 h-10 bg-gray-100 shadow-md rounded-md hover:bg-gray-200'>
+            프로필 편집
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
