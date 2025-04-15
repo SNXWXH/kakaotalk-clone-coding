ae37eca211585e48c6a73d7ba70299ec742bb959
SNXWXH <snxwxh@gmail.com>
2025-04-15 15:26:52 +0900

    feat: profile 컴포넌트 생성 및 라우터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import './App.css';
 import Layout from './common/Layout';
 import LoginPage from './pages/login/LoginPage';
 import RegisterPage from './pages/register/RegisterPage';
-import ChatList from './pages/chatList/ChatList';
-import Chat from './pages/chat/Chat';
+import ChatListPage from './pages/chatList/ChatListPage';
+import ChatPage from './pages/chat/ChatPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,8 +16,9 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path='/' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/chatList' element={<ChatList />} />
-          <Route path='/chat' element={<Chat />} />
+          <Route path='/chatList' element={<ChatListPage />} />
+          <Route path='/chat' element={<ChatPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </Layout>
---
+++ src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,11 @@
+function ProfilePage() {
+  return (
+    <>
+      <div className='flex w-[392px] h-[642px] bg-[#FEFEFE] text-sm rounded-xl'>
+        profile
+      </div>
+    </>
+  );
+}
+
+export default ProfilePage;
