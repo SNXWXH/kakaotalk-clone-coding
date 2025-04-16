3fa6b1e9175c4920dea5072e78da4172bda09ed9
SNXWXH <snxwxh@gmail.com>
2025-04-16 13:21:58 +0900

    modify: tailwind.config.js 파일 제거

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  plugins: ['@tailwindcss/postcss'],
-};
-export default config;
