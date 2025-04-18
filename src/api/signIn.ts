bffbf99edf458d724cb54ceb620a5b41618ea384
SNXWXH <snxwxh@gmail.com>
2025-04-18 16:50:27 +0900

    feat: 로그인 API 타입 정의 및 적용

--- src/api/signIn.ts
+++ src/api/signIn.ts
@@ -1,3 +1,4 @@
+import { LoginData } from '../types';
 import api from './axios';
 
-export const login = (loginData) => api.post('/signin', loginData);
+export const login = (loginData: LoginData) => api.post('/signin', loginData);
