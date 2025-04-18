3bc42442551ee154a76094f18da25a0426888448
SNXWXH <snxwxh@gmail.com>
2025-04-18 18:04:23 +0900

    refactor: 라우팅 코드 main.tsx에서 app.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css';
-// import LoginPage from './pages/login/LoginPage';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './common/Layout';
+import LoginPage from './pages/login/LoginPage';
+import RegisterPage from './pages/register/RegisterPage';
+import ChatListPage from './pages/chatList/ChatListPage';
+import ChatPage from './pages/chat/ChatPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
   return (
-    <>
-      <h1>root</h1>
-    </>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/chatlist' element={<ChatListPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Layout from './common/Layout';
-import LoginPage from './pages/login/LoginPage';
-import RegisterPage from './pages/register/RegisterPage';
-import ChatListPage from './pages/chatList/ChatListPage';
-import ChatPage from './pages/chat/ChatPage';
-import ProfilePage from './pages/profile/ProfilePage';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/chatlist' element={<ChatListPage />} />
-          <Route path='/chat' element={<ChatPage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
-    </Layout>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
