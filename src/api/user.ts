840b56af610c9f3851aad5ea07d30d9989600c51
SNXWXH <snxwxh@gmail.com>
2025-04-29 23:24:55 +0900

    feat: 프로필 API 연동 및 타입 정의

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,15 @@
-import { LoginData, RegisterData } from '../types';
+import { LoginData, ProfileData, RegisterData } from '../types';
 import api from './axios';
 
 export const login = (loginData: LoginData) => api.post('/signin', loginData);
+
 export const register = (registerData: RegisterData) =>
   api.post('/signup', registerData);
+
+export const profile = (profileData: ProfileData) => {
+  api.patch('/users/me', profileData, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+};
