031b75cad07e9c5196bf96889a6021893ee8c99c
SNXWXH <snxwxh@gmail.com>
2025-04-29 17:41:42 +0900

    feat: msgData, chatInfo props 적용 및 렌더링 로직 구현

--- src/common/ChatItem.tsx
+++ src/common/ChatItem.tsx
@@ -1,34 +1,64 @@
+import { ChatRoom, Message } from '../types';
+import formatTime from '../utils/formatTime';
 import ChatBubble from './ChatBubble';
 
-export default function ChatItem() {
-  const isSender = [false, true, true, false, false, true, true, false, false];
+export default function ChatItem({
+  msgData,
+  chatInfo,
+}: {
+  msgData: Message[];
+  chatInfo: ChatRoom | null;
+}) {
+  const curUserId = Number(localStorage.getItem('id')) || '';
 
   return (
     <>
-      {isSender.reverse().map((sender, i) => (
+      {msgData.map((msg, i) => (
         <div
           key={i}
           className={`flex ${
-            sender ? 'justify-end' : 'justify-start'
+            msg.sender_id === curUserId ? 'justify-end' : 'justify-start'
           } items-start gap-2 w-full`}
         >
-          {!sender && (
-            <img src={'/userImg.png'} className='w-12 h-12 rounded-md' />
+          {msg.sender_id !== curUserId && (
+            <>
+              <img
+                src={
+                  chatInfo?.other_user?.profile_image_url ||
+                  '/default-profile.png'
+                }
+                className='w-12 h-12 rounded-md'
+              />
+            </>
           )}
 
           <div
-            className={`flex items-end gap-1 ${sender ? 'justify-end' : ''}`}
+            className={`flex items-end gap-1 ${
+              msg.sender_id === curUserId ? 'justify-end' : ''
+            }`}
           >
-            {sender && (
+            {msg.sender_id === curUserId && (
               <>
-                <p className='text-xs text-gray-400 mb-1'>19:20</p>
-                <ChatBubble isSender={sender} />
+                <p className='text-xs text-gray-400 mb-1'>
+                  {formatTime(msg.created_at)}
+                </p>
+                <ChatBubble
+                  isSender={msg.sender_id === curUserId}
+                  msg={msg.content}
+                />
               </>
             )}
-            {!sender && (
+
+            {msg.sender_id !== curUserId && (
               <>
-                <ChatBubble isSender={sender} />
-                <p className='text-xs text-gray-400 mb-1'>19:20</p>
+                <ChatBubble
+                  isSender={msg.sender_id === curUserId}
+                  msg={msg.content}
+                  otherUser={chatInfo?.other_user?.name}
+                />
+                <p className='text-xs text-gray-400 mb-1'>
+                  {formatTime(msg.created_at)}
+                </p>
               </>
             )}
           </div>
