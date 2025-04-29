6ccb6a21833909657f9d9e95a5c2dc5ca3389b00
SNXWXH <snxwxh@gmail.com>
2025-04-29 17:34:53 +0900

    feat: 송수신자 선택 라디오 버튼 및 메세지 전송 기능 추가
    '나'와 '상대방'을 선택할 수 있는 라디오 버튼 추가 및 senderType 상태 관리
    선택된 송신자에 따라 sender_id 동적으로 설정하여 메시지 전송
    채팅방 정보 및 채팅 내역 API 연동
    ChatItem 컴포넌트에 채팅 데이터 및 방 정보 props로 전달

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -1,34 +1,115 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import ChatItem from '../../common/ChatItem';
+import { ChangeEvent, useEffect, useState } from 'react';
+import {
+  getChatInfo,
+  getChatMsg,
+  getChatMyMsg,
+  postChatMsg,
+} from '../../api/chat';
+import { ChatRoom } from '../../types';
 
 function Chat() {
+  const [content, setContent] = useState('');
+  const [msg, setMsg] = useState([]);
+  const [chatInfo, setChatInfo] = useState<ChatRoom | null>(null);
+  const [senderType, setSenderType] = useState<'me' | 'other'>('me');
+
   const navigate = useNavigate();
+  const { id } = useParams();
+
+  const sender_id = Number(localStorage.getItem('id'));
 
   const handleImg = () => {
     navigate('/chatlist');
   };
+
+  const inputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  const senderChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value as 'me' | 'other';
+    setSenderType(value);
+  };
+
+  const sendMsg = async () => {
+    const chatroomId = id || '';
+
+    const senderId = senderType === 'me' ? sender_id : Number(id);
+
+    const sendData = {
+      sender_id: senderId,
+      content,
+    };
+
+    await postChatMsg({ chatroomId, sendData });
+  };
+
+  useEffect(() => {
+    const getChat = async () => {
+      let res;
+      if (!id) return;
+      id === 'me' ? (res = await getChatMyMsg()) : (res = await getChatMsg(id));
+      let info = await getChatInfo(id);
+
+      setMsg(res.data);
+      setChatInfo(info.data);
+    };
+
+    getChat();
+  }, []);
 
   return (
     <>
       <div className='flex flex-col w-[392px] h-[642px] bg-[#c1d6e8] text-sm rounded-xl'>
         <header className='relative flex h-16 items-center'>
           <img
-            src={'./left.svg'}
-            className='absolute left-4 h-6'
+            src={'/left.svg'}
+            className='absolute left-4 h-6 cursor-pointer'
             onClick={handleImg}
           />
-          <p className='mx-auto text-xl font-bold'>이설아</p>
+          <p className='mx-auto text-xl font-bold'>
+            {chatInfo?.other_user?.name}
+          </p>
         </header>
-        <div className='flex flex-col-reverse overflow-y-auto h-3/4 px-2 gap-2'>
-          <ChatItem />
+
+        <div className='flex flex-col overflow-y-auto h-3/4 p-2 gap-2'>
+          <ChatItem msgData={msg} chatInfo={chatInfo} />
+        </div>
+        <div className='flex justify-center gap-4 p-2 bg-stone-100'>
+          <label className='flex items-center gap-1 '>
+            <input
+              type='radio'
+              name='sender'
+              value='me'
+              checked={senderType === 'me'}
+              onChange={senderChange}
+            />
+            나
+          </label>
+          <label className='flex items-center gap-1 h-7'>
+            <input
+              type='radio'
+              name='sender'
+              value='other'
+              checked={senderType === 'other'}
+              onChange={senderChange}
+            />
+            상대방
+          </label>
         </div>
         <div className='h-1/5 relative'>
           <textarea
             name='chat'
             placeholder='메세지를 입력하세요'
-            className='h-full w-full resize-none bg-[#FEFEFE] outline-none p-2 pr-16 '
+            className='h-full w-full resize-none bg-[#FEFEFE] outline-none p-2 pr-16'
+            onChange={inputChange}
           />
-          <button className='absolute bottom-2 right-2 z-10 h-10 w-14 rounded-md bg-gray-200'>
+          <button
+            onClick={sendMsg}
+            className='absolute bottom-2 right-2 z-10 h-10 w-14 rounded-md bg-gray-200'
+          >
             입력
           </button>
         </div>
