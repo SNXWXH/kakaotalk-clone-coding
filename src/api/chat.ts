42a4cde67bef2cebb088551c7ae1992763ee60ee
SNXWXH <snxwxh@gmail.com>
2025-04-29 16:22:39 +0900

    feat: 나와의 채팅, 채팅방 대화, 채팅 보내기, 채팅방 정보 API 연동

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -7,3 +7,41 @@ export const getChatRoom = () =>
       Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
   });
+
+//나와의 채팅 대화 불러오기
+export const getChatMyMsg = () =>
+  api.get('/chatrooms/me/chats', {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+
+//채팅방 대화 불러오기
+export const getChatMsg = (chatroomId: string) =>
+  api.get(`/chatrooms/${chatroomId}/chats`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+
+//채팅 보내기
+export const postChatMsg = ({
+  chatroomId,
+  sendData,
+}: {
+  chatroomId: string;
+  sendData: { sender_id: number; content: string };
+}) =>
+  api.post(`/chatrooms/${chatroomId}/chats`, sendData, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+
+//채팅방 정보 불러오기
+export const getChatInfo = (chatroomId: string) =>
+  api.get(`/chatrooms/${chatroomId}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
