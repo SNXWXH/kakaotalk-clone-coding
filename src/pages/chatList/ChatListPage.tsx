a6383ba8b4fb5ae5094dec9c0424455b02cac0a0
SNXWXH <snxwxh@gmail.com>
2025-04-29 23:59:31 +0900

    fix: chatListPage vercel 내 오류 수정

--- src/pages/chatList/ChatListPage.tsx
+++ src/pages/chatList/ChatListPage.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import ChatListItem from '../../common/ChatListItem';
 import { useEffect, useState } from 'react';
 import { getChatRoom } from '../../api/chat';
-import { ChatRoom } from '../../types';
+import { ChatRoom, UserInfo } from '../../types';
 import { getUserInfo } from '../../api/user';
 
 function ChatList() {
@@ -40,16 +40,16 @@ function ChatList() {
           <div className='flex justify-between items-center h-20 bg-[#FEFEFE] hover:bg-gray-100'>
             <div className='flex items-center justify-center h-[60px] w-[70px] ml-4'>
               <img
-                src={userInfo.profile_image_url}
+                src={userInfo?.profile_image_url}
                 onClick={handleImg}
                 className='h-full w-[60px] rounded-xl'
               />
             </div>
             <div className='flex flex-col w-3/5 h-3/4 justify-center px-2'>
               <p className='h-3/5 flex items-center font-bold text-[16px]'>
-                {userInfo.name}
+                {userInfo?.name}
               </p>
-              <p className='h-2/5 flex items-center'>{userInfo.bio}</p>
+              <p className='h-2/5 flex items-center'>{userInfo?.bio}</p>
             </div>
             <button
               className='flex justify-center items-center w-1/4 h-9 bg-gray-200 m-3.5 p-2 rounded-2xl cursor-pointer'
