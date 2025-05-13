67b30f2af36e7167fd819c32a2f611bbe4865e97
SNXWXH <snxwxh@gmail.com>
2025-05-13 16:36:27 +0900

    feat: axios 인터셉터 추가로 토큰 자동 설정

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,28 +1,14 @@
 import api from './axios';
 
 //채팅방 목록 불러오기
-export const getChatRoom = () =>
-  api.get('/users/me/chatrooms', {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+export const getChatRoom = () => api.get('/users/me/chatrooms');
 
 //나와의 채팅 대화 불러오기
-export const getChatMyMsg = () =>
-  api.get('/chatrooms/me/chats', {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+export const getChatMyMsg = () => api.get('/chatrooms/me/chats');
 
 //채팅방 대화 불러오기
 export const getChatMsg = (chatroomId: string) =>
-  api.get(`/chatrooms/${chatroomId}/chats`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+  api.get(`/chatrooms/${chatroomId}/chats`);
 
 //채팅 보내기
 export const postChatMsg = ({
@@ -31,17 +17,8 @@ export const postChatMsg = ({
 }: {
   chatroomId: string;
   sendData: { sender_id: number; content: string };
-}) =>
-  api.post(`/chatrooms/${chatroomId}/chats`, sendData, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+}) => api.post(`/chatrooms/${chatroomId}/chats`, sendData);
 
 //채팅방 정보 불러오기
 export const getChatInfo = (chatroomId: string) =>
-  api.get(`/chatrooms/${chatroomId}`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+  api.get(`/chatrooms/${chatroomId}`);
--- src/api/user.ts
+++ src/api/user.ts
@@ -6,16 +6,7 @@ export const login = (loginData: LoginData) => api.post('/signin', loginData);
 export const register = (registerData: RegisterData) =>
   api.post('/signup', registerData);
 
-export const getUserInfo = () =>
-  api.get('/users/me', {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+export const getUserInfo = () => api.get('/users/me');
 
 export const profileUpdate = (profileData: ProfileData) =>
-  api.patch('/users/me', profileData, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
+  api.patch('/users/me', profileData);
